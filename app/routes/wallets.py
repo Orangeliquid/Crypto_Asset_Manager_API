87d92c9a6c9faf727dd68df029ac88b918482537
Orangeliquid <hardym2020111@gmail.com>
2025-03-11 04:31:34 -0400

    Change names of functions from get to fetch, change response_model in create_wallet to WalletBase, remove value as param and change asset_symbol to coin_name for add_asset_to_wallet, Add delete_wallet route

--- app/routes/wallets.py
+++ app/routes/wallets.py
@@ -1,18 +1,36 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from typing import List
 
-from app.schemas.wallets import WalletResponse
+from app.schemas.wallets import WalletBase, WalletResponse, WalletDeleteResponse
 from app.schemas.assets import AssetResponse
-from app.crud.wallets import crud_create_wallet, crud_add_asset_to_wallet, crud_get_wallet_by_id
+from app.crud.wallets import crud_create_wallet, crud_add_asset_to_wallet, crud_get_wallet_by_id, crud_delete_wallet
+from app.crud.wallets import crud_get_all_wallets
 from app.database import get_db
 
 router = APIRouter()
 
 
 # Route to create a wallet
-@router.post("/users/{user_id}/wallet/", response_model=WalletResponse)
+@router.post("/users/{user_id}/wallet/", response_model=WalletBase)
 def create_wallet(user_id: int, db: Session = Depends(get_db)):
     return crud_create_wallet(db=db, user_id=user_id)
+
+
+# Route to get wallet details (including assets)
+@router.get("/users/{user_id}/wallet/{wallet_id}/", response_model=WalletResponse)
+def fetch_wallet(user_id: int, wallet_id: int, db: Session = Depends(get_db)):
+    return crud_get_wallet_by_id(db=db, user_id=user_id, wallet_id=wallet_id)
+
+
+@router.get("/wallets/", response_model=List[WalletResponse])
+def fetch_all_wallets(db: Session = Depends(get_db)):
+    wallets = crud_get_all_wallets(db)
+
+    if not wallets:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No wallets found")
+
+    return wallets
 
 
 # Route to add an asset to a wallet
@@ -20,24 +38,20 @@ def create_wallet(user_id: int, db: Session = Depends(get_db)):
 def add_asset_to_wallet(
     user_id: int,
     wallet_id: int,
-    asset_symbol: str,
+    coin_name: str,
     quantity: float,
-    value_usd: float,
     db: Session = Depends(get_db)
 ):
-    # Call the CRUD function to add or update asset
-    asset = crud_add_asset_to_wallet(
+    return crud_add_asset_to_wallet(
         db=db,
         user_id=user_id,
         wallet_id=wallet_id,
-        asset_symbol=asset_symbol,
+        coin_name=coin_name,
         quantity=quantity,
-        value_usd=value_usd
     )
-    return asset
 
 
-# Route to get wallet details (including assets)
-@router.get("/{wallet_id}", response_model=WalletResponse)
-def get_wallet(wallet_id: int, db: Session = Depends(get_db)):
-    return crud_get_wallet_by_id(db=db, wallet_id=wallet_id)
+@router.delete("/users/{user_id}/wallet/{wallet_id}/", response_model=WalletDeleteResponse)
+def delete_wallet(user_id: int, wallet_id: int, db: Session = Depends(get_db)):
+    delete_message = crud_delete_wallet(db=db, wallet_id=wallet_id, user_id=user_id)
+    return {"message": delete_message}
