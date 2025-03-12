f1499fa6157aab8f19c8be1313bdbd95bc5af65c
Orangeliquid <hardym2020111@gmail.com>
2025-03-12 04:40:11 -0400

    Add PurchaseTransaction and SaleTransaction models, update relationships of previous models to reflect new models added

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime
 from sqlalchemy.orm import relationship
+from datetime import datetime
 
 from app.database import Base
 
@@ -12,7 +13,9 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     password_hash = Column(String)
 
-    wallets = relationship("Wallet", back_populates="owner")
+    wallets = relationship("Wallet", back_populates="user")
+    purchase_transactions = relationship("PurchaseTransaction", back_populates="user")
+    sale_transactions = relationship("SaleTransaction", back_populates="user")
 
 
 class Wallet(Base):
@@ -21,8 +24,10 @@ class Wallet(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
 
-    owner = relationship("User", back_populates="wallets")
+    user = relationship("User", back_populates="wallets")
     assets = relationship("Asset", back_populates="wallet")
+    purchase_transactions = relationship("PurchaseTransaction", back_populates="wallet")
+    sale_transactions = relationship("SaleTransaction", back_populates="wallet")
 
     @property
     def amount_of_coins(self):
@@ -45,3 +50,41 @@ class Asset(Base):
     value_usd = Column(Float, default=0.0)
 
     wallet = relationship("Wallet", back_populates="assets")
+    purchase_transactions = relationship("PurchaseTransaction", back_populates="asset")
+    sale_transactions = relationship("SaleTransaction", back_populates="asset")
+
+
+class PurchaseTransaction(Base):
+    __tablename__ = "purchase_transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    wallet_id = Column(Integer, ForeignKey("wallets.id"))
+    asset_id = Column(Integer, ForeignKey("assets.id"))
+    coin_name = Column(String)
+    quantity_purchased = Column(Float)
+    purchase_price = Column(Float)
+    total_purchase_price = Column(Float)
+    purchase_date = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="purchase_transactions")
+    wallet = relationship("Wallet", back_populates="purchase_transactions")
+    asset = relationship("Asset", back_populates="purchase_transactions")
+
+
+class SaleTransaction(Base):
+    __tablename__ = "sale_transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    wallet_id = Column(Integer, ForeignKey("wallets.id"))
+    asset_id = Column(Integer, ForeignKey("assets.id"))
+    coin_name = Column(String)
+    quantity_sold = Column(Float)
+    sale_price = Column(Float)
+    total_sale_price = Column(Float)
+    sale_date = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="sale_transactions")
+    wallet = relationship("Wallet", back_populates="sale_transactions")
+    asset = relationship("Asset", back_populates="sale_transactions")
