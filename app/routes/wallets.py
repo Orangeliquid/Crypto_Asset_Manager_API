22f44a4ff323878330c2308160c7d8b29e653655
Orangeliquid <hardym2020111@gmail.com>
2025-03-12 04:41:46 -0400

    Add get_transactions_for_wallet route and sell_asset route

--- app/routes/wallets.py
+++ app/routes/wallets.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Query
 from sqlalchemy.orm import Session
 from typing import List
 
 from app.schemas.wallets import WalletBase, WalletResponse, WalletDeleteResponse
-from app.schemas.assets import AssetResponse
-from app.crud.wallets import crud_create_wallet, crud_add_asset_to_wallet, crud_get_wallet_by_id, crud_delete_wallet
+from app.schemas.transactions import PurchaseTransactionResponse, SaleTransactionResponse, PaginatedTransactionsResponse
+from app.schemas.transactions import Transaction
+from app.crud.wallets import crud_create_wallet, crud_purchase_asset, crud_sell_asset
+from app.crud.wallets import crud_get_wallet_by_id, crud_get_transactions_for_wallet, crud_delete_wallet
 from app.crud.wallets import crud_get_all_wallets
 from app.database import get_db
 
@@ -33,16 +35,57 @@ def fetch_all_wallets(db: Session = Depends(get_db)):
     return wallets
 
 
-# Route to add an asset to a wallet
-@router.put("/users/{user_id}/wallet/{wallet_id}/add_asset", response_model=AssetResponse)
-def add_asset_to_wallet(
+@router.get("/users/{user_id}/wallet/{wallet_id}/transactions", response_model=PaginatedTransactionsResponse)
+def get_transactions_for_wallet(
+        user_id: int,
+        wallet_id: int,
+        limit: int = Query(10, ge=1),
+        page: int = Query(1, ge=1),
+        db: Session = Depends(get_db)
+):
+    transactions, total_transactions, total_pages, current_page = crud_get_transactions_for_wallet(
+        db=db,
+        user_id=user_id,
+        wallet_id=wallet_id,
+        limit=limit,
+        page=page
+    )
+
+    # Return the response directly using the response model
+    return PaginatedTransactionsResponse(
+        transactions=transactions,
+        total_transactions=total_transactions,
+        total_pages=total_pages,
+        current_page=current_page
+    )
+
+
+@router.put("/users/{user_id}/wallet/{wallet_id}/purchase_asset", response_model=PurchaseTransactionResponse)
+def purchase_asset(
     user_id: int,
     wallet_id: int,
     coin_name: str,
     quantity: float,
     db: Session = Depends(get_db)
 ):
-    return crud_add_asset_to_wallet(
+    return crud_purchase_asset(
+        db=db,
+        user_id=user_id,
+        wallet_id=wallet_id,
+        coin_name=coin_name,
+        quantity=quantity,
+    )
+
+
+@router.put("/users/{user_id}/wallet/{wallet_id}/sell_asset", response_model=SaleTransactionResponse)
+def sell_asset(
+    user_id: int,
+    wallet_id: int,
+    coin_name: str,
+    quantity: float,
+    db: Session = Depends(get_db)
+):
+    return crud_sell_asset(
         db=db,
         user_id=user_id,
         wallet_id=wallet_id,
