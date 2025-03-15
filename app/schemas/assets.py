13aecb7fb210bc025682b651d47afc98b9df4354
Orangeliquid <hardym2020111@gmail.com>
2025-03-15 04:46:59 -0400

    Add new attributes to AssetResponse class for better description of coin data fetched from Coin Cap API

--- app/schemas/assets.py
+++ app/schemas/assets.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 
 class AssetBase(BaseModel):
@@ -17,7 +17,21 @@ class AssetCreate(AssetBase):
 
 
 class AssetResponse(AssetBase):
+    coin_cap_id: str
+    coin_cap_rank: int
+    coin_cap_symbol: str
+
+    current_price_usd: float
+    current_value_usd: float
+    net_gain_loss: float
     initial_purchase_date: datetime
+    market_cap_usd: float
+    supply: float
+    max_supply: Optional[float] = 0.0
+    volume_usd_24hr: float
+    change_percent_24hr: float
+    vwap_24hr: Optional[float] = 0.0
+    explorer_url: Optional[str] = "Missing URL from CoinCapAPI"
     id: int
 
     class Config:
@@ -28,7 +42,7 @@ class PaginatedAssetsResponse(BaseModel):
     total_count: int
     total_pages: int
     current_page: int
-    total_wallet_value: float
+    total_wallet_value_usd: float
     assets: List[AssetResponse]
 
     class Config:
